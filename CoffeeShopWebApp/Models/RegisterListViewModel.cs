c6d78b6f9d28ccc671f37cafde11e2585f1241a0
sean <armstrong.d.sean@gmail.com>
2018-08-13 17:32:12 -0400

    validates that data is required using jquery and the model lamda method

--- CoffeeShopWebApp/Models/RegisterListViewModel.cs
+++ CoffeeShopWebApp/Models/RegisterListViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace CoffeeShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopWebApp.Models
 {
     public class RegisterListViewModel
     {
@@ -7,11 +9,23 @@
 
         }
 
+        [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Favorite coffee is required")]
         public string FavoriteCoffee { get; set; }
+
+        [Required(ErrorMessage = "Birthday is required")]
         public string Birthday { get; set; }
+
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
     }
 }
