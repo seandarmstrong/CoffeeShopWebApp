930b7b4c8d6d0941290f2ddccac4c580ca8753b4
sean <armstrong.d.sean@gmail.com>
2018-08-13 19:59:18 -0400

    all validations are working and program is working as instructed

--- CoffeeShopWebApp/Models/RegisterListViewModel.cs
+++ CoffeeShopWebApp/Models/RegisterListViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoffeeShopWebApp.Models
 {
@@ -10,22 +11,26 @@ namespace CoffeeShopWebApp.Models
         }
 
         [Required(ErrorMessage = "First name is required")]
+        [MinLength(3)]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Last name is required")]
+        [MinLength(3)]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Favorite coffee is required")]
         public string FavoriteCoffee { get; set; }
 
         [Required(ErrorMessage = "Birthday is required")]
-        public string Birthday { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? Birthday { get; set; }
 
         [Required(ErrorMessage = "Email address is required")]
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
+        [StringLength(15, ErrorMessage = "Password must be between {2} and {1} characters long.", MinimumLength = 6)]
         public string Password { get; set; }
     }
 }
