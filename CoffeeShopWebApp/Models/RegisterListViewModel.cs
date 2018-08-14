a2c1ba0b29e89fd573727b0c724918d5cd590c67
sean <armstrong.d.sean@gmail.com>
2018-08-13 20:45:52 -0400

    validated another way using jquery and commented out of the old way

--- CoffeeShopWebApp/Models/RegisterListViewModel.cs
+++ CoffeeShopWebApp/Models/RegisterListViewModel.cs
@@ -10,27 +10,27 @@ namespace CoffeeShopWebApp.Models
 
         }
 
-        [Required(ErrorMessage = "First name is required")]
-        [MinLength(3)]
+        //[Required(ErrorMessage = "First name is required")]
+        //[MinLength(3)]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Last name is required")]
-        [MinLength(3)]
+        //[Required(ErrorMessage = "Last name is required")]
+        //[MinLength(3)]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Favorite coffee is required")]
+        //[Required(ErrorMessage = "Favorite coffee is required")]
         public string FavoriteCoffee { get; set; }
 
-        [Required(ErrorMessage = "Birthday is required")]
-        [DataType(DataType.Date)]
+        //[Required(ErrorMessage = "Birthday is required")]
+        //[DataType(DataType.Date)]
         public DateTime? Birthday { get; set; }
 
-        [Required(ErrorMessage = "Email address is required")]
-        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        //[Required(ErrorMessage = "Email address is required")]
+        //[EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
-        [StringLength(15, ErrorMessage = "Password must be between {2} and {1} characters long.", MinimumLength = 6)]
+        //[Required(ErrorMessage = "Password is required")]
+        //[StringLength(15, ErrorMessage = "Password must be between {2} and {1} characters long.", MinimumLength = 6)]
         public string Password { get; set; }
     }
 }
