94c1b9acff13d909533af233837ad9f5b0d185c1
sean <armstrong.d.sean@gmail.com>
2018-08-14 11:29:17 -0400

    minor changes and started using the first HTML attributes validation method

--- CoffeeShopWebApp/Models/RegisterListViewModel.cs
+++ CoffeeShopWebApp/Models/RegisterListViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoffeeShopWebApp.Models
 {
@@ -10,27 +11,34 @@ namespace CoffeeShopWebApp.Models
         }
 
         //creates the properties for the class. the commented out validations are for the second way to validate
-        //[Required(ErrorMessage = "First name is required")]
-        //[MinLength(3)]
+        [Required(ErrorMessage = "First name is required")]
+        [MinLength(3)]
         public string FirstName { get; set; }
 
-        //[Required(ErrorMessage = "Last name is required")]
-        //[MinLength(3)]
+        [Required(ErrorMessage = "Last name is required")]
+        [MinLength(3)]
         public string LastName { get; set; }
 
-        //[Required(ErrorMessage = "Favorite coffee is required")]
+        [Required(ErrorMessage = "Favorite coffee is required")]
         public string FavoriteCoffee { get; set; }
 
-        //[Required(ErrorMessage = "Birthday is required")]
-        //[DataType(DataType.Date)]
+        [Required(ErrorMessage = "Birthday is required")]
+        [DataType(DataType.Date)]
         public DateTime? Birthday { get; set; }
 
-        //[Required(ErrorMessage = "Email address is required")]
-        //[EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        //[CustomEmail]
         public string Email { get; set; }
 
-        //[Required(ErrorMessage = "Password is required")]
-        //[StringLength(15, ErrorMessage = "Password must be between {2} and {1} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(15, ErrorMessage = "Password must be between {2} and {1} characters long.", MinimumLength = 6)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        [StringLength(15, ErrorMessage = "Password must be between {2} and {1} characters long.", MinimumLength = 6)]
+        [Compare("Password", ErrorMessage = "The password you're entering must match the password you entered above")]
+        public string ConfirmPassword { get; set; }
+
     }
 }
---
+++ CoffeeShopWebApp/Validator/CustomEmailAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace CoffeeShopWebApp.Models
+{
+    public class CustomEmailAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var email = value.ToString();
+            var emailRegex = new Regex("\\W+@\\w.\\w");
+
+            var isValidEmail = emailRegex.IsMatch(email);
+            if (isValidEmail)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("This is an invalid email address.");
+            }
+
+            
+        }
+    }
+}
