5314f5a9b0dd29e02e295e12091ebaf149dbcf2a
sean <armstrong.d.sean@gmail.com>
2018-08-15 17:20:38 -0400

    having shopping cart working and now need to add images and make look good

--- CoffeeShopWebApp/Helpers/Constants.cs
+++ CoffeeShopWebApp/Helpers/Constants.cs
@@ -4,5 +4,6 @@
     {
         public const string FirstNameCookie = "FirstNameCookie";
         public const string FavoriteCoffeeCookie = "FavoriteCoffeeCookie";
+        public const string CounterCookie = "CounterCookie";
     }
 }
